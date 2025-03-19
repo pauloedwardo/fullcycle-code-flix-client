a2bd1b66d832799391f28f8a6de6999f897d5dfa
Paulo Eduardo <plo.erd@gmail.com>
2025-03-19 07:02:00 -0300

    Start to implement course lesson

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
+      './src/app/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        textColor: {
+            white: '#FFF'
+        }
+    }
+    // plugins: [
+    //   require('tailwind-scrollbar-hide'),
+    //   require('@vidstack/react/tailwind.cjs'),
+    // ],
+  };
